200f637577d341b48b3a28749d9c0d61074ed3cc
lythesia <iranaikimi@gmail.com>
2016-06-01 20:00:44 +0800

    hello-world.scm: opt

--- scheme/hello-world/hello-world.scm
+++ scheme/hello-world/hello-world.scm
@@ -2,6 +2,6 @@
   #:export (hello))
 
 ;; define* or lambda* would allow opt-arg
-(define* (hello #:optional name)
-  (string-concatenate (list "Hello, " (or name "World") "!"))
+(define* (hello #:optional (name "World"))
+  (string-concatenate (list "Hello, " name "!"))
 )
