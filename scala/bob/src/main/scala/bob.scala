b662a11a3b361d9e6ecd22b5f479d6937ec4de3d
lythesia <iranaikimi@gmail.com>
2016-08-05 09:53:09 +0800

    round2

--- scala/bob/src/main/scala/bob.scala
+++ scala/bob/src/main/scala/bob.scala
@@ -1,16 +1,19 @@
 class Bob {
-  def blank(s: String) = s.isEmpty
-  def shout(s: String) = {
+  private def blank(s: String) = s.isEmpty
+
+  private def shout(s: String) = {
     val s1 = s.toUpperCase
     val s2 = s.toLowerCase
     s == s1 && s1 != s2
   }
-  def question(s: String) = s.last == '?'
 
-  def hey(s: String) = s.trim match {
-    case s if blank(s) => "Fine. Be that way!"
-    case s if shout(s) => "Whoa, chill out!"
-    case s if question(s) => "Sure."
-    case _ => "Whatever."
+  private def question(s: String) = s.endsWith("?")
+
+  def hey(s: String) = {
+    val ts = s.trim
+    if(blank(ts)) "Fine. Be that way!"
+    else if(shout(ts)) "Whoa, chill out!"
+    else if(question(ts)) "Sure."
+    else "Whatever."
   }
 }
--- scala/hamming/src/main/scala/hamming.scala
+++ scala/hamming/src/main/scala/hamming.scala
@@ -1,8 +1,6 @@
 object Hamming {
   def compute(x: String, y: String) = {
-    if(x.length == y.length)
-      x.zip(y).count(p => p._1 != p._2)
-    else
-      throw new IllegalArgumentException(s"not equal length: $x <-> $y")
+    require(x.length == y.length, s"not equal length: $x <-> $y")
+    x.zip(y).count(p => p._1 != p._2)
   }
 }
