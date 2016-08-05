f297bdde407fd2fb96a469bb915bb8b89590e559
lythesia <iranaikimi@gmail.com>
2016-08-05 11:11:24 +0800

    case partial func

--- scala/hamming/src/main/scala/hamming.scala
+++ scala/hamming/src/main/scala/hamming.scala
@@ -1,6 +1,6 @@
 object Hamming {
   def compute(x: String, y: String) = {
     require(x.length == y.length, s"not equal length: $x <-> $y")
-    x.zip(y).count(p => p._1 != p._2)
+    x.zip(y).count { case (c1, c2) => c1 != c2 }
   }
 }
