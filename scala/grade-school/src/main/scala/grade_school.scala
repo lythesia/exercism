41b0e43026a45fba04a2257304283f577d15b9d9
lythesia <iranaikimi@gmail.com>
2016-08-17 14:44:14 +0800

    grade-school.scala

---
+++ scala/grade-school/src/main/scala/grade_school.scala
@@ -0,0 +1,11 @@
+class School {
+  private val _db = scala.collection.mutable.Map[Int, List[String]]()
+
+  def add(n: String, g: Int) = _db(g) = _db.getOrElseUpdate(g, List()) :+ n
+
+  def grade(g: Int) = _db.getOrElse(g, Nil)
+
+  def db = _db.toMap
+
+  def sorted = scala.collection.immutable.TreeMap(_db.mapValues(_.sorted).toSeq :_*)
+}
