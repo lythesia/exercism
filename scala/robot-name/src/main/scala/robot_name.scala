5a077076e515e2eae69ddfb693705286ff6d2574
lythesia <iranaikimi@gmail.com>
2016-08-18 19:08:12 +0800

    robot-name.scala

---
+++ scala/robot-name/src/main/scala/robot_name.scala
@@ -0,0 +1,12 @@
+import scala.util.Random.{alphanumeric => alnum}
+
+class Robot {
+
+  var _name = gen
+
+  private def gen = (alnum.filter(_.isUpper).take(2) ++ alnum.filter(_.isDigit).take(3)).mkString
+
+  def name = _name
+
+  def reset(): Unit = _name = gen
+}
