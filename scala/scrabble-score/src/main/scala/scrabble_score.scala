0bcf6c4db2f3c7720e77297948fb1ae8d40c870b
lythesia <iranaikimi@gmail.com>
2016-08-30 17:22:14 +0800

    scrabble-score.scala

---
+++ scala/scrabble-score/src/main/scala/scrabble_score.scala
@@ -0,0 +1,14 @@
+class Scrabble {
+  def scoreWord(word: String) = word map scoreLetter sum
+
+  def scoreLetter(ch: Char) = ch.toUpper match {
+    case 'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T'  => 1
+    case 'D' | 'G' => 2
+    case 'B' | 'C' | 'M' | 'P' => 3
+    case 'F' | 'H' | 'V' | 'W' | 'Y' => 4
+    case 'K' => 5
+    case 'J' | 'X' => 8
+    case 'Q' | 'Z' => 10
+    case _ => 0
+  }
+}
