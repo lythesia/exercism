e5b189c4da4d12b8e155dd475cb5649891764b84
lythesia <iranaikimi@gmail.com>
2016-08-30 10:15:12 +0800

    memorized

--- scala/grains/src/main/scala/grains.scala
+++ scala/grains/src/main/scala/grains.scala
@@ -1,4 +1,13 @@
-object Grains {
-  def square(x: Int) = BigInt(1) << (x - 1)
-  val total = (BigInt(1) << 64) - 1
+object Grains extends WithCache[Int, BigInt] {
+  private def doSquare(x: Int) = BigInt(1) << (x - 1)
+
+  def square(x: Int) = withCache(x, doSquare)
+
+  def total = (1 to 64).map(square).sum
+}
+
+trait WithCache[K,V] {
+  private val cache = collection.mutable.Map[K,V]()
+
+  def withCache(key: K, f: K => V) = cache.getOrElseUpdate(key, f(key))
 }
